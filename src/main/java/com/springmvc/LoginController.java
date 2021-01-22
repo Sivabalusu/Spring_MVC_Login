c9787d0c8470676f03131504965204662e4ec442
Siva Balusu <Sbalusu1882@conestogac.on.ca>
2021-01-22 01:12:00 -0500

    Created sample class and returnnig content from it through @requestmapping and @ResponseBody

---
+++ src/main/java/com/springmvc/LoginController.java
@@ -0,0 +1,17 @@
+package com.springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+//We don't need to extend HTTPServlet if we specify @Controller
+public class LoginController {
+	
+	@RequestMapping(value = "/login")
+	@ResponseBody
+	public String sayHello() {
+		return "Hello";
+	}
+
+}
